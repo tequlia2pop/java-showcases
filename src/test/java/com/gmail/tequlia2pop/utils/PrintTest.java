24f337c90872b5d6ef4b32474dd64100cf4f4236
tequlia2pop <tequlia2pop@gmail.com>
2017-12-09 15:17:35 +0800

    Add print util test

---
+++ src/test/java/com/gmail/tequlia2pop/utils/PrintTest.java
@@ -0,0 +1,13 @@
+package com.gmail.tequlia2pop.utils;
+
+import static com.gmail.tequlia2pop.utils.Print.print;
+import static com.gmail.tequlia2pop.utils.Print.println;
+
+public class PrintTest {
+	public static void main(String[] args) {
+		println("Available from now on!");
+		print(100 + ", ");
+		print(100L + ", ");
+		println(3.14159);
+	}
+}
